f3acb1aca1c715d89f8e6dc74c601fa6b6fa4360
vblong <bolong.v@gmail.com>
2022-02-07 11:12:54 +0100

    LDA topic modelling code in notebook and visualization

--- task1-topic-modelling-lda/task1.py
+++ task1-topic-modelling-lda/task1.py
@@ -1,96 +1,118 @@
 #source https://towardsdatascience.com/topic-modelling-in-python-with-nltk-and-gensim-4ef03213cd21
 
 #   Spacy
-import spacy
-from spacy.lang.en import English
+# import spacy
+# from spacy.lang.en import English
 
 #   NLTK
-import nltk
-from nltk.corpus import wordnet as wn
-from nltk.stem.wordnet import WordNetLemmatizer
+# import nltk
+# from nltk.corpus import wordnet as wn
+# from nltk.stem.wordnet import WordNetLemmatizer
 
 #   Gensim
-from gensim import corpora
+# from gensim import corpora
 
-#   Util
-import random
+#   Numpy
+# import numpy as np
+#
+# #   Util
+# import random
+#
+# #   Additional packages
+from utils import pipeline
 
-#   Additional packages
-from utils import tokenize, get_lemma, get_lemma2, prepare_text_for_lda
+model = pipeline()
 
+# spacy.load('en_core_web_sm')
+# parser = English()
 
-spacy.load('en_core_web_sm')
-parser = English()
-nltk.download('wordnet')
+# nltk.download('wordnet')
 
-wnLemmatizer = WordNetLemmatizer()
-for w in ['dogs', 'ran', 'discouraged']:
-    print(w, get_lemma(w, wn), get_lemma2(w, wnLemmatizer))
+# wnLemmatizer = WordNetLemmatizer()
+# print("######## TESTING LEMMATIZATION #######")
+# for w in ['dogs', 'ran', 'discouraged']:
+#     print(w, get_lemma(w, wn), get_lemma2(w, wnLemmatizer))
+#
+# nltk.download('stopwords')
+# en_stop = set(nltk.corpus.stopwords.words('english'))
+# print("######## READ SOME DATA FROM ./data/dataset.csv #######")
+# #   Read sample data
+# text_data = []
+# with open('./data/dataset.csv') as f:
+#     for line in f:
+#         tokens = prepare_text_for_lda(line, en_stop, parser, wn)
+#         if random.random() > .99:
+#             print(line, tokens)
+#             text_data.append(tokens)
+#
+# dictionary = corpora.Dictionary(text_data)
+#
+# print("Total lines read:")
+# print(len(text_data))
+#
+# corpus = [dictionary.doc2bow(text) for text in text_data]
+#
+# print("######## TAKE A LOOK AT THE 'dictionary' #######")
+# for k in dictionary:
+#     print(k, dictionary[k])
+#
+# print("######## TAKE A LOOK AT THE 'corpus' #######")
+# for anything, i in enumerate(corpus):
+#     print(anything, text_data[anything], i)
+#
+# import pickle
+# pickle.dump(corpus, open('corpus.pkl', 'wb'))
+# dictionary.save('dictionary.gensim')
+#
+# import gensim
+# NUM_TOPICS = 5
+# ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = NUM_TOPICS, id2word=dictionary, passes=15)
+# ldamodel.save('model5.gensim')
+#
+# print(f'######## HERE ARE THE {NUM_TOPICS} TOPICS: #######')
+# topics = ldamodel.print_topics(num_words=4)
+# for topic in topics:
+#     print(topic)
+#
+# new_doc = 'Practical Bayesian Optimization of Machine Learning Algorithms'
+# new_doc = prepare_text_for_lda(new_doc, en_stop, parser, wn)
+# new_doc_bow = dictionary.doc2bow(new_doc)
+# print(f'####### NOW TEST THIS SENTENCE {new_doc_bow}')
+# potential_topic = ldamodel.get_document_topics(new_doc_bow)
+# highest_among_them = max(range(len(potential_topic)), key=lambda i: potential_topic[i][1])
+# print(f'The topics distribution is: ', potential_topic)
+# print(f'So the possible topic is: ', max(potential_topic, key=lambda x: x[1]), f' index {highest_among_them}')
+# print(f'Equivalent to {topics[highest_among_them]}')
 
-nltk.download('stopwords')
-en_stop = set(nltk.corpus.stopwords.words('english'))
-
-#   Read sample data
-text_data = []
-with open('./data/dataset.csv') as f:
-    for line in f:
-        tokens = prepare_text_for_lda(line, en_stop)
-        if random.random() > .99:
-            print(tokens)
-            text_data.append(tokens)
-
-dictionary = corpora.Dictionary(text_data)
-
-corpus = [dictionary.doc2bow(text) for text in text_data]
-
-import pickle
-pickle.dump(corpus, open('corpus.pkl', 'wb'))
-dictionary.save('dictionary.gensim')
-
-import gensim
-NUM_TOPICS = 5
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = NUM_TOPICS, id2word=dictionary, passes=15)
-ldamodel.save('model5.gensim')
-
-topics = ldamodel.print_topics(num_words=4)
-for topic in topics:
-    print(topic)
-
-new_doc = 'Practical Bayesian Optimization of Machine Learning Algorithms'
-new_doc = prepare_text_for_lda(new_doc)
-new_doc_bow = dictionary.doc2bow(new_doc)
-print(new_doc_bow)
-print(ldamodel.get_document_topics(new_doc_bow))
-
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = 3, id2word=dictionary, passes=15)
-ldamodel.save('model3.gensim')
-topics = ldamodel.print_topics(num_words=4)
-for topic in topics:
-    print(topic)
-
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = 10, id2word=dictionary, passes=15)
-ldamodel.save('model10.gensim')
-topics = ldamodel.print_topics(num_words=4)
-for topic in topics:
-    print(topic)
-
-dictionary = gensim.corpora.Dictionary.load('dictionary.gensim')
-corpus = pickle.load(open('corpus.pkl', 'rb'))
-lda = gensim.models.ldamodel.LdaModel.load('model5.gensim')
-
-import pyLDAvis
-import pyLDAvis.gensim_models as gensimvis
-lda_display = gensimvis.prepare(lda, corpus, dictionary, sort_topics=False)
-pyLDAvis.save_html(lda_display, 'LDA_Visualization.html')
-# pyLDAvis.display(lda_display)
-
-lda3 = gensim.models.ldamodel.LdaModel.load('model3.gensim')
-lda_display3 = gensimvis.prepare(lda3, corpus, dictionary, sort_topics=False)
-pyLDAvis.save_html(lda_display, 'LDA_Visualization3.html')
-# pyLDAvis.display(lda_display3)
-
-
-lda10 = gensim.models.ldamodel.LdaModel.load('model10.gensim')
-lda_display10 = gensimvis.prepare(lda10, corpus, dictionary, sort_topics=False)
-pyLDAvis.save_html(lda_display, 'LDA_Visualization10.html')
-# pyLDAvis.display(lda_display10)
+# ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = 3, id2word=dictionary, passes=15)
+# ldamodel.save('model3.gensim')
+# topics = ldamodel.print_topics(num_words=4)
+# for topic in topics:
+#     print(topic)
+#
+# ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = 10, id2word=dictionary, passes=15)
+# ldamodel.save('model10.gensim')
+# topics = ldamodel.print_topics(num_words=4)
+# for topic in topics:
+#     print(topic)
+#
+# dictionary = gensim.corpora.Dictionary.load('dictionary.gensim')
+# corpus = pickle.load(open('corpus.pkl', 'rb'))
+# lda = gensim.models.ldamodel.LdaModel.load('model5.gensim')
+#
+# import pyLDAvis
+# import pyLDAvis.gensim_models as gensimvis
+# lda_display = gensimvis.prepare(lda, corpus, dictionary, sort_topics=False)
+# pyLDAvis.save_html(lda_display, 'LDA_Visualization.html')
+# # pyLDAvis.display(lda_display)
+#
+# lda3 = gensim.models.ldamodel.LdaModel.load('model3.gensim')
+# lda_display3 = gensimvis.prepare(lda3, corpus, dictionary, sort_topics=False)
+# pyLDAvis.save_html(lda_display, 'LDA_Visualization3.html')
+# # pyLDAvis.display(lda_display3)
+#
+#
+# lda10 = gensim.models.ldamodel.LdaModel.load('model10.gensim')
+# lda_display10 = gensimvis.prepare(lda10, corpus, dictionary, sort_topics=False)
+# pyLDAvis.save_html(lda_display, 'LDA_Visualization10.html')
+# # pyLDAvis.display(lda_display10)
--- task1-topic-modelling-lda/utils.py
+++ task1-topic-modelling-lda/utils.py
@@ -1,7 +1,53 @@
+# Pandas
+import pandas as pd
 
-"""
-    Convert a string into a list of token
-"""
+# Spacy
+import spacy
+from spacy.lang.en import English
+
+# NLTK
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.stem.wordnet import WordNetLemmatizer
+
+# Gensim
+import gensim
+from gensim import corpora
+
+# Numpy
+import numpy as np
+
+# Pickle
+import pickle
+
+# Util
+import random
+spacy.load('en_core_web_sm')
+parser = English()
+
+nltk.download('wordnet')
+
+wnLemmatizer = WordNetLemmatizer()
+
+nltk.download('stopwords')
+en_stop = set(nltk.corpus.stopwords.words('english'))
+
+parser = English()
+
+def read_data(file_name, nrows=None):
+    if isinstance(file_name, list):
+        data = pd.DataFrame()
+        for file in file_name:
+            chunk = pd.read_csv(file, nrows=nrows)
+            data = data.append(chunk, ignore_index=True)
+    else:
+        data = pd.read_csv(file_name, nrows=nrows)
+    return data
+
+def extract_document_body(data):
+    bodys = [row['body'] for index, row in data.iterrows()]
+    return bodys
+
 def tokenize(text, parser):
     lda_tokens = []
     tokens = parser(text)
@@ -16,44 +62,82 @@ def tokenize(text, parser):
             lda_tokens.append(token.lower_)
     return lda_tokens
 
-"""
-    Get the basic form of a word
-    Params:
-        word: word to be converted
-        wn: lemmatizer
-    E.g: 
-        Input: learning 
-        Output: learn
-"""
-def get_lemma(word, wn):
+
+def lemmatize_1(word, wn):
     lemma = wn.morphy(word)
     if lemma is None:
         return word
     else:
         return lemma
 
-"""
-    Get the basic form of a word
-    Params:
-        word: word to be converted
-        wn: lemmatizer
-    E.g: 
-        Input: learning 
-        Output: learn
-"""
-def get_lemma2(word, lemmatizer):
+def lemmatize_2(word, lemmatizer):
     return lemmatizer.lemmatize(word)
 
-"""
-    Preprocessing texts
-    1.  Tokenize string into tokens
-    2.  Keep only token of length > 4
-    3.  Remove stopwords
-    4.  Lemmatizing the rest
-"""
-def prepare_text_for_lda(text, stop_words):
-    tokens = tokenize(text)
+def preprocess(sentence, stop_words, parser, wordnet):
+    tokens = tokenize(sentence, parser)
     tokens = [token for token in tokens if len(token) > 4]
     tokens = [token for token in tokens if token not in stop_words]
-    tokens = [get_lemma(token) for token in tokens]
+    tokens = [lemmatize_1(token, wordnet) for token in tokens]
     return tokens
+
+def save_model(model, file_name='Task1_LDA.gensim'):
+    model.save(filename)
+    print(f'Model is saved `{filename}`')
+
+def save_corpus(corpus, filename='Task1_corpus.pkl'):
+    pickle.dump(corpus, open(filename, 'wb'))
+    print(f'Corpus saved as `{filename}`')
+
+def save_dictionary(dictionary, filename='Task1_dictionary.gensim'):
+    dictionary.save(filename)
+    print(f'Dictionary saved as `{filename}`')
+
+
+def train_lda(corpus, num_topics=15, filename='task1_lda.gensim', save=True, print_topics=True, print_num_topics=15,
+              print_num_words=4):
+    print(f'Start modelling topics with Gensim\'s LDA, corpus length = {len(corpus)}')
+    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=num_topics, id2word=dictionary, passes=15)
+    if save == True:
+        ldamodel.save(filename)
+        print(f'LDA built with 15 topics and saved as `{filename}`')
+
+    if print_topics == True:
+        topics = ldamodel.print_topics(num_topics=print_num_topics, num_words=print_num_words)
+        print(f'Here are the top {print_num_topics} with #{print_num_words} words per topic')
+        for topic in topics:
+            print(topic)
+
+    return ldamodel
+
+
+def print_topics(model, print_num_topics=15, print_num_words=4):
+    topics = model.print_topics(num_topics=print_num_topics, num_words=print_num_words)
+    print(f'Here are the top {print_num_topics} with #{print_num_words} words per topic')
+    for topic in topics:
+        print(topic)
+
+def pipeline(save=True):
+    # Step 1: read data
+    data = read_data(["data/polusa_balanced/2017_1.csv"], nrows=1000000)
+
+    # Step 2 - extract main content from each article
+    documents = extract_document_body(data)
+
+    # Step 3 - preprocessing
+    text_data = [preprocess(body, en_stop, parser, wn) for body in documents]
+
+    # Step 4 - build dictionary
+    dictionary = corpora.Dictionary(text_data)
+
+    # Step 5 - build corpus
+    corpus = [dictionary.doc2bow(text) for text in text_data]
+
+    # Step 6 - run LDA
+    model = train_lda(corpus)
+
+    if save == True:
+        save_corpus(corpus)
+        save_dictionary(dictionary)
+        save_model(model)
+
+    return model
