8c941f8db065d2960c14da1a16b862533af65314
Long Vu <bolong.v@gmail.com>
2022-12-07 19:15:31 +0100

    Polarity classification trainning and validation on Polusa 6k

--- main.py
+++ main.py
@@ -1,122 +1,200 @@
-#source https://towardsdatascience.com/topic-modelling-in-python-with-nltk-and-gensim-4ef03213cd21
-import task1
+import pandas as pd
+import torch, numpy as np
+
+from os.path import join
+from transformers import BertTokenizer, BertModel
+from modules.preprocessing.io import write_pickle
+from tqdm import tqdm
+
+DATA_FOLDER = './data/polusa_polarity_balanced_6k'
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+labels = {
+    'LEFT': 0,
+    'CENTER': 1,
+    'RIGHT': 2
+}
+    
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, df):
+
+        self.labels = [labels[label] for label in df['political_leaning']]
+        self.texts = [tokenizer(text, 
+                               padding='max_length', max_length = 512, truncation=True,
+                                return_tensors="pt") for text in df['body']]
+
+    def classes(self):
+        return self.labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_batch_labels(self, idx):
+        # Fetch a batch of labels
+        return np.array(self.labels[idx])
+
+    def get_batch_texts(self, idx):
+        # Fetch a batch of inputs
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+
+        return batch_texts, batch_y
 
 
-#
-# import spacy
-# spacy.load('en_core_web_sm')
-# from spacy.lang.en import English
-# parser = English()
-#
-# def tokenize(text):
-#     lda_tokens = []
-#     tokens = parser(text)
-#     for token in tokens:
-#         if token.orth_.isspace():
-#             continue
-#         elif token.like_url:
-#             lda_tokens.append('URL')
-#         elif token.orth_.startswith('@'):
-#             lda_tokens.append('SCREEN_NAME')
-#         else:
-#             lda_tokens.append(token.lower_)
-#     return lda_tokens
-#
-# import nltk
-# nltk.download('wordnet')
-#
-# from nltk.corpus import wordnet as wn
-#
-#
-# def get_lemma(word):
-#     lemma = wn.morphy(word)
-#     if lemma is None:
-#         return word
-#     else:
-#         return lemma
-#
-#
-# from nltk.stem.wordnet import WordNetLemmatizer
-#
-#
-# def get_lemma2(word):
-#     return WordNetLemmatizer().lemmatize(word)
-#
-# for w in ['dogs', 'ran', 'discouraged']:
-#     print(w, get_lemma(w), get_lemma2(w))
-#
-# nltk.download('stopwords')
-# en_stop = set(nltk.corpus.stopwords.words('english'))
-#
-# def prepare_text_for_lda(text):
-#     tokens = tokenize(text)
-#     tokens = [token for token in tokens if len(token) > 4]
-#     tokens = [token for token in tokens if token not in en_stop]
-#     tokens = [get_lemma(token) for token in tokens]
-#     return tokens
-#
-# import random
-# text_data = []
-# with open('task1-topic-modelling-lda/data/dataset.csv') as f:
-#     for line in f:
-#         tokens = prepare_text_for_lda(line)
-#         if random.random() > .99:
-#             print(tokens)
-#             text_data.append(tokens)
-#
-# from gensim import corpora
-# dictionary = corpora.Dictionary(text_data)
-#
-# corpus = [dictionary.doc2bow(text) for text in text_data]
-#
-# import pickle
-# pickle.dump(corpus, open('corpus.pkl', 'wb'))
-# dictionary.save('dictionary.gensim')
-#
-# import gensim
-# NUM_TOPICS = 5
-# ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = NUM_TOPICS, id2word=dictionary, passes=15)
-# ldamodel.save('model5.gensim')
-#
-# topics = ldamodel.print_topics(num_words=4)
-# for topic in topics:
-#     print(topic)
-#
-# new_doc = 'Practical Bayesian Optimization of Machine Learning Algorithms'
-# new_doc = prepare_text_for_lda(new_doc)
-# new_doc_bow = dictionary.doc2bow(new_doc)
-# print(new_doc_bow)
-# print(ldamodel.get_document_topics(new_doc_bow))
-#
-# ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = 3, id2word=dictionary, passes=15)
-# ldamodel.save('model3.gensim')
-# topics = ldamodel.print_topics(num_words=4)
-# for topic in topics:
-#     print(topic)
-#
-# ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = 10, id2word=dictionary, passes=15)
-# ldamodel.save('model10.gensim')
-# topics = ldamodel.print_topics(num_words=4)
-# for topic in topics:
-#     print(topic)
-#
-# dictionary = gensim.corpora.Dictionary.load('dictionary.gensim')
-# corpus = pickle.load(open('corpus.pkl', 'rb'))
-# lda = gensim.models.ldamodel.LdaModel.load('model5.gensim')
-#
-# import pyLDAvis
-# import pyLDAvis.gensim_models as gensimvis
-# lda_display = gensimvis.prepare(lda, corpus, dictionary, sort_topics=False)
-# pyLDAvis.save_html(lda_display, 'LDA_Visualization.html')
-# # pyLDAvis.display(lda_display)
-#
-# lda3 = gensim.models.ldamodel.LdaModel.load('model3.gensim')
-# lda_display3 = gensimvis.prepare(lda3, corpus, dictionary, sort_topics=False)
-# pyLDAvis.save_html(lda_display, 'LDA_Visualization3.html')
-# # pyLDAvis.display(lda_display3)
-#
-#
-# lda10 = gensim.models.ldamodel.LdaModel.load('model10.gensim')
-# lda_display10 = gensimvis.prepare(lda10, corpus, dictionary, sort_topics=False)
-# pyLDAvis.save_html(lda_display, 'LDA_Visualization10.html')
-# # pyLDAvis.display(lda_display10)
+class BertClassifier(torch.nn.Module):
+
+    def __init__(self, dropout=0.5):
+
+        super(BertClassifier, self).__init__()
+
+        self.bert = BertModel.from_pretrained('bert-base-cased')
+        self.dropout = torch.nn.Dropout(dropout)
+        self.linear = torch.nn.Linear(768, 5)
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, input_id, mask):
+
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
+
+
+def polarity_classification_train(model, train_data, val_data, learning_rate, epochs):
+
+    train, val = Dataset(train_data), Dataset(val_data)
+
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=2, shuffle=True)
+    val_dataloader = torch.utils.data.DataLoader(val, batch_size=2)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr= learning_rate)
+
+    if use_cuda:
+
+            model = model.cuda()
+            criterion = criterion.cuda()
+
+    for epoch_num in range(epochs):
+
+            total_acc_train = 0
+            total_loss_train = 0
+
+            for train_input, train_label in tqdm(train_dataloader):
+
+                train_label = train_label.to(device)
+                mask = train_input['attention_mask'].to(device)
+                input_id = train_input['input_ids'].squeeze(1).to(device)
+
+                output = model(input_id, mask)
+                
+                batch_loss = criterion(output, train_label.long())
+                total_loss_train += batch_loss.item()
+                
+                acc = (output.argmax(dim=1) == train_label).sum().item()
+                total_acc_train += acc
+
+                model.zero_grad()
+                batch_loss.backward()
+                optimizer.step()
+            
+            total_acc_val = 0
+            total_loss_val = 0
+
+            with torch.no_grad():
+
+                for val_input, val_label in val_dataloader:
+
+                    val_label = val_label.to(device)
+                    mask = val_input['attention_mask'].to(device)
+                    input_id = val_input['input_ids'].squeeze(1).to(device)
+
+                    output = model(input_id, mask)
+
+                    batch_loss = criterion(output, val_label.long())
+                    total_loss_val += batch_loss.item()
+                    
+                    acc = (output.argmax(dim=1) == val_label).sum().item()
+                    total_acc_val += acc
+            
+            print(
+                f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
+                | Train Accuracy: {total_acc_train / len(train_data): .3f} \
+                | Val Loss: {total_loss_val / len(val_data): .3f} \
+                | Val Accuracy: {total_acc_val / len(val_data): .3f}')
+                
+# Read Polusa dataset
+def polarity_read_data(files):
+    li = []
+    for filename in files:
+        print(f'=== Reading {filename}')
+        df = pd.read_csv(join(DATA_FOLDER, filename), index_col=None, header=0)
+        li.append(df)
+
+    df = pd.concat(li, axis=0, ignore_index=True)
+
+    return df
+
+# Remove UNKNOWN polarity leaning
+def polarity_clean_unknown_leaning(df):
+    print('Before remove UNKNOWN leaning: ', len(df))
+    df = df.drop(df[df.political_leaning == 'UNDEFINED'].index)
+    print('After remove UNKNOWN leaning: ', len(df))    
+    return df
+
+def polarity_classification_evaluate(model, test_data):
+
+    test = Dataset(test_data)
+
+    test_dataloader = torch.utils.data.DataLoader(test, batch_size=2)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    if use_cuda:
+
+        model = model.cuda()
+
+    total_acc_test = 0
+    with torch.no_grad():
+
+        for test_input, test_label in test_dataloader:
+
+              test_label = test_label.to(device)
+              mask = test_input['attention_mask'].to(device)
+              input_id = test_input['input_ids'].squeeze(1).to(device)
+
+              output = model(input_id, mask)
+
+              acc = (output.argmax(dim=1) == test_label).sum().item()
+              total_acc_test += acc
+    
+    print(f'Test Accuracy: {total_acc_test / len(test_data): .3f}')
+
+if __name__ == "__main__":
+    files = ['data.csv']
+
+    df = polarity_read_data(files)
+    df = polarity_clean_unknown_leaning(df)
+
+    df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42), 
+                                     [int(.8*len(df)), int(.9*len(df))])
+
+    print(f'Train size: {len(df_train)} \nValidation size: {len(df_val)} \nTest size: {len(df_test)}')
+
+    EPOCHS = 5
+    model = BertClassifier()
+    LR = 1e-6
+                
+    polarity_classification_train(model, df_train, df_val, LR, EPOCHS)
+    polarity_classification_evaluate(model, df_test)
+
+    write_pickle('./model/bert_classifier_polusa_balanced_6k.pkl', model)
